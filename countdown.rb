7ba2a3fbf11b1701cf2cd96f23c0b88b7785b5c6
Pancho Labrador <plabradorjr@gmail.com>
2019-10-06 22:15:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown(x)
 end
 
 
-def countdown_with_sleep
+def countdown_with_sleep(x)
   while x > 0
     puts "#{x} SECOND(S)!"
-    sleep(1)
     x -= 1
+    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
